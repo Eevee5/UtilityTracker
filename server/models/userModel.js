c612959a188d6d0fc8ac8352b1b7fbf07e6d401f
Wanlu D <46799027+WanluD@users.noreply.github.com>
2023-10-03 19:45:51 -0400

    ready to merge with frontend

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const User = new Schema({
-  username: { type: String, require: true },
-  password: { type: String, require: true },
-  securityQuestion: { type: String, require: true },
-  securityAnswer: { type: String, require: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  securityQuestion: { type: String, required: true },
+  securityAnswer: { type: String, required: true },
+  // Potential fields for OAuth
+  oauthProvider: { type: String },
+  oauthId: { type: String },
+
 });
 
 module.exports = mongoose.model('User', User);
